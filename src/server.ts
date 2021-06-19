b78503e793d5fcc4d042b0e19e707f2ab02584c4
Thiago <thiagoalves060@gmail.com>
2021-06-19 19:33:39 -0300

    Primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello world" });
+});
+
+app.post("/coursers", (req, res) => {
+  const { name } = req.body;
+  return res.json({ name });
+});
+
+app.listen(3333, () => console.log("Server is runner"));
