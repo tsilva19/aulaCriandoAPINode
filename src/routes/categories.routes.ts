dcef0048609a1cef2d07658b0a67b1c13f51183a
Thiago <thiagoalves060@gmail.com>
2021-08-08 14:09:36 -0300

    Aula Solid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,8 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository()
@@ -9,14 +11,10 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (req, res) =>{
   const {name, description} = req.body;
   
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if(categoryAlreadyExists){
-    return res.status(400).json({error: "Category Already exists!!" })
-  }
-
-  categoriesRepository.create({ name, description})
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
   
+  createCategoryService.execute({name, description});
+
   return res.status(201).send();
 });
 
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,29 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * Definir o tipo de retorno
+ * Alterar o retorno de erro
+ * Acessar o repositorio
+ * Retornar algo
+ */
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
